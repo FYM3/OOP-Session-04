d6ca65ae3d0810d37b647d15fe4c115c6fb3eb7d
FYM3 <yousefmohame8i8@gmail.com>
2026-08-17 17:29:32 +0300

    8  Main() Checklist

--- Assignment#05-OOP#04/Program.cs
+++ Assignment#05-OOP#04/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace Assignment_05_OOP_04
+﻿using Assignment_05_OOP_02.Delivery_Address;
+using Assignment_05_OOP_02.Delivery_Center;
+using Assignment_05_OOP_02.Express_Shipment;
+using Assignment_05_OOP_02.International_Shipment;
+using Assignment_05_OOP_02.Standard_Shipment;
+
+namespace Assignment_05_OOP_04
 {
     internal class Program
     {
@@ -37,6 +43,84 @@
             // Answer c)
             // No, a class cannot inherit from multiple Abstract Classes.
             // Yes, a class can implement multiple Interfaces.
+
+            #endregion
+
+
+            #region 8Q) => Main() Checklist 
+
+            // a) Create StandardShipment
+            StandardShipment standardShipment = new StandardShipment("SH001", "Laptop", 5, 70, new DeliveryAddress());
+
+            // b) Create ExpressShipment
+            ExpressShipment expressShipment = new ExpressShipment("SH002", "Phone", 0, 70, new DeliveryAddress(), 30);
+
+            // c) Create InternationalShipment
+            InternationalShipment internationalShipment = new InternationalShipment("SH003", "Monitor", 20, 50, new DeliveryAddress(), "Germany", 110);
+
+            // Delivery Center
+            DeliveryCenter deliveryCenter = new DeliveryCenter();
+
+            // d) Add all shipments
+            deliveryCenter.AddShipment(standardShipment);
+            deliveryCenter.AddShipment(expressShipment);
+            deliveryCenter.AddShipment(internationalShipment);
+
+
+
+            // e) Print all shipment details
+            Console.WriteLine("==========================================");
+            Console.WriteLine("Delivery Center");
+            Console.WriteLine("==========================================");
+            deliveryCenter.PrintAllShipments();
+
+
+            // f) Print Tracking Status
+            Console.WriteLine();
+            Console.WriteLine("==========================================");
+            Console.WriteLine("Tracking Status");
+            Console.WriteLine("==========================================");
+            deliveryCenter.PrintTrackingStatuses();
+
+
+            // g) Print Insurance
+            Console.WriteLine();
+            Console.WriteLine("==========================================");
+            Console.WriteLine("Insurance");
+            Console.WriteLine("==========================================");
+            deliveryCenter.PrintCalculateInsurance();
+
+
+
+            // h) ITrackable[] Polymorphism
+            Console.WriteLine();
+            Console.WriteLine("==========================================");
+            Console.WriteLine("ITrackable[] Polymorphism");
+            Console.WriteLine("==========================================");
+
+            ITrackable.ITrackable[] trackableShipments = { standardShipment, expressShipment, internationalShipment };
+
+            foreach (ITrackable.ITrackable shipment in trackableShipments)
+                Console.WriteLine(shipment.GetTrackingStatus());
+
+
+
+            // i) IInsurable[] Polymorphism
+            Console.WriteLine();
+            Console.WriteLine("==========================================");
+            Console.WriteLine("IInsurable[] Polymorphism");
+            Console.WriteLine("==========================================");
+
+            IInsurable.IInsurable[] insurableShipments = { standardShipment, expressShipment, internationalShipment };
+
+            foreach (IInsurable.IInsurable shipment in insurableShipments)
+                Console.WriteLine(shipment.CalculateInsurance());
+
+            Console.WriteLine();
+            Console.WriteLine("==========================================");
+            Console.WriteLine("Interface Polymorphism Demonstrated Successfully.");
+            Console.WriteLine("==========================================");
+
 
             #endregion
 
