737799fe6aadca4cc9e32ea58d1f4423a2f3282c
FYM3 <yousefmohame8i8@gmail.com>
2026-08-17 17:03:47 +0300

    6  Create DeliveryReport

---
+++ Assignment#05-OOP#04/Delivery Report/DeliveryReport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+
+namespace Assignment_05_OOP_04.Delivery_Report
+{
+    public class DeliveryReport
+    {
+        public void PrintShipment(ITrackable.ITrackable shipment)
+        {
+            Console.WriteLine(shipment.GetTrackingStatus());
+        }
+
+        public void PrintInsurance(IInsurable.IInsurable shipment)
+        {
+            Console.WriteLine(shipment.CalculateInsurance());
+        }
+    }
+}
